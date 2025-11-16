e0bcce6b936b1c4ad87ac52d11773f8cee480929
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2025-11-16 11:14:56 +0530

    hihi

--- numpy1.py
+++ numpy1.py
@@ -1,8 +1,13 @@
 import numpy as np
-data_type=[('name','S15'),('class',int),('height', float)]
-students_details = [('mahi',7,5.1),('vruddhi',8,5.4),('johan',7,4.9),('myna',7,5)]
-students=np.array(students_details,dtype=data_type)
-print("original array :")
-print(students)
-print("sort by height")
-print(np.sort(students, order='height'))
+ogarray = np.linspace(0, 9, 10, dtype=int)
+print("original array:")
+print(ogarray)
+new = np.where(ogarray % 2 != 0, -1, ogarray)
+print("\nmodified array (odd numbers replaced with -1):")
+print(new)
+two_d_array = ogarray.reshape(2, -1)
+print("\n2D array (2 rows):")
+print(two_d_array)
+sum_all = np.sum(ogarray)
+print("\nsum of all numbers in original array:")
+print(sum_all)
